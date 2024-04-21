86bcc9c59b3c99802db1caa1eee58ad006064484
Rabbyte <quangtuantran1324@gmail.com>
2024-04-21 16:13:18 +0700

    First setup

---
+++ app/src/main/java/com/rabbyte/onlineshop/activities/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.rabbyte.onlineshop.activities
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rabbyte.onlineshop.R
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+}
---
+++ app/src/main/java/com/rabbyte/onlineshop/activities/IntroActivity.kt
@@ -0,0 +1,33 @@
+package com.rabbyte.onlineshop.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rabbyte.onlineshop.R
+import com.rabbyte.onlineshop.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
